a88442efef4f021e6266498bc5dceb1fdbc2c2bd
太田有紀 <51269927+ifuriito@users.noreply.github.com>
2019-12-02 20:01:45 +0900

    Update 0002.C

--- 0002.C
+++ 0002.C
@@ -4,12 +4,13 @@ int main(void)
 {
 	int data[] = {83,42,39,79,13,75,19};
 	int i,sum = 0,avg;
+	int array_count = sizeof(data) / sizeof(data[0]);
 	
-	for (i = 0;i < sizeof(data) / sizeof(data[0]);i++) {
+	for (i = 0;i < array_count;i++) {
 		sum += data[i];
 	}
 	
-	avg = sum / (sizeof(data) / sizeof(data[0]));
+	avg = sum / array_count ;
 	
 	printf("%d\n",avg);
 	
